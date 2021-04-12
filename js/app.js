c5d8f61e45bebbffe18261459c4d67b63f4cc045
Cristian Castañeda <crisleoco14@gmail.com>
2021-04-12 11:46:59 -0500

    Button deleted

--- js/app.js
+++ js/app.js
@@ -7,50 +7,48 @@ resultado.hide();
 
 $(document).ready(function () {
 
-    var name = prompt("Cual es tu nombre?");
+    var name = prompt("Cual es tu nombre?");  //Agrega el nombre
     $('#nombre-us').html(name);
 
-    $('.start-button').click(function() { 
+    $('.start-button').click(function() {  
 
-        $('.start-button').html('Reiniciar');
+        //$('.start-button').html('Reiniciar');
+        $('.start-button').hide();
 
         var variable;
         var usuario;
         var scoreUser = 0;
         var scorePc = 0;
 
-        if (scorePc > 0 || scoreUser == 0) {
-            scorePc = 0;
-            scoreUser = 0;
-            $('.user-score').html(scoreUser);
-            $('.cpu-score').html(scorePc);
-            $('.seleccion-us').attr('src', './img/interrogacion.svg');
-            $('.seleccion-pc').attr('src', './img/interrogacion.svg');
-        }
+        $('.user-score').html(scoreUser);
+        $('.cpu-score').html(scorePc);
+        $('.seleccion-us').attr('src', './img/interrogacion.svg');
+        $('.seleccion-pc').attr('src', './img/interrogacion.svg');
+        
 
 
         $('.button-piedra').click(function(){
             usuario = 1;
-            console.log(usuario);
+            console.log('seleccion usuario: ' + usuario);
             variable = Math.floor(Math.random()*(3-1+1)+1);
             variable = parseInt(variable);
-            console.log("El valor de la computadora fue " + variable);
+            console.log("El valor de la computadora fue: " + variable);
             game(usuario, variable);
         });
         $('.button-papel').click(function(){
             usuario = 2;
-            console.log(usuario);
+            console.log('seleccion usuario: ' + usuario);
             variable = Math.floor(Math.random()*(3-1+1)+1);
             variable = parseInt(variable);
-            console.log("El valor de la computadora fue " + variable);
+            console.log("El valor de la computadora fue: " + variable);
             game(usuario, variable);
         });
         $('.button-tijeras').click(function(){
             usuario = 3;
-            console.log(usuario);
+            console.log('seleccion usuario: ' + usuario);
             variable = Math.floor(Math.random()*(3-1+1)+1);
             variable = parseInt(variable);
-            console.log("El valor de la computadora fue " + variable);
+            console.log("El valor de la computadora fue: " + variable);
             game(usuario, variable);
         });
 
@@ -68,26 +66,26 @@ $(document).ready(function () {
             } else if (usuario == 1 && variable != 2 ) {
                 scoreUser+=1;
                 $('.user-score').html(scoreUser);
-                console.log(scoreUser);
+                console.log('puntaje usuario: ' + scoreUser);
                 $('.result').html('Ganaste!');
                 animation();
             } else if (usuario == 2 && variable != 3 ) {
                 scoreUser+=1;
                 $('.user-score').html(scoreUser);
-                console.log(scoreUser);
+                console.log('puntaje usuario: ' + scoreUser);
                 $('.result').html('Ganaste!');
                 animation();
             } else if (usuario == 3 && variable != 1 ) {
                 scoreUser+=1;
                 $('.user-score').html(scoreUser);
-                console.log(scoreUser);
+                console.log('puntaje usuario: ' + scoreUser);
                 $('.result').html('Ganaste!');
                 animation();
             } else 
             {
                 scorePc+=1;
                 $('.cpu-score').html(scorePc);
-                console.log(scorePc);
+                console.log('puntaje cp: ' + scorePc);
                 $('.result').html('Perdiste');
                 animation();
             }
@@ -122,6 +120,7 @@ $(document).ready(function () {
             }
 
         }
+        
     });
 });
 
